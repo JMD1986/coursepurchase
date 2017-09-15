65f53c0390203db21a46e08a44f22fb196c76f00
John Daugherty <johndaugherty1986@gmail.com>
2017-09-15 16:27:45 -0500

    creating data

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import Coursesales from './Coursesales';
 
 class App extends Component {
   render() {
+    var courses = [
+      { name: "Complete iOS10 dev course", price: 199 },
+      { name: "Complete Rails 5 course", price: 199 },
+      { name: "Complete React course", price: 199 },
+      { name: "Complete Phoenix", price: 199 }
+    ];
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Course Purchases Page</h2>
         </div>
-        <Coursesales />
+        <Coursesales items={courses}/>
       </div>
     );
   }
--- src/coursesales.js
+++ src/coursesales.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class Coursesales extends Component {
   render () {
+    console.log(this.props.items);
     return (
       <div>
       I am from course Sales page.
