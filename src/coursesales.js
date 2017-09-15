b12534103ff0d181a7588c293a31be7f76ec42fe
John Daugherty <johndaugherty1986@gmail.com>
2017-09-14 23:37:25 -0500

    added coursesales component

---
+++ src/coursesales.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+Class Coursesales extends Component {
+  render () {
+    return (
+      <div>
+      I am from course Sales page.
+      </div>
+    );
+  }
+}
+
+export default Coursesales;
