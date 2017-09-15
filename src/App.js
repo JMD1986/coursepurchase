eba190d603f1dddc869f6e37457d1459f27b89fe
John Daugherty <johndaugherty1986@gmail.com>
2017-09-15 16:20:25 -0500

    fixed type

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Coursesales from './coursesales';
+import Coursesales from './Coursesales';
 
 class App extends Component {
   render() {
